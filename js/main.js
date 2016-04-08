9eb1cb6ad1055a15daccb009b8b802b9611dc8cb
Shannon <shaw0106@algonquinlive.com>
2016-04-08 15:48:05 -0400

    added js file and js waypoint

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var $biglockin = $('.biglockin');
+var $lineupin = $('.lineupin');
+var $chartin = $('.chartin');
+var $coinin = $('.coinin');
+var $bandsin = $('.bandsin');
+var $lockbottomin = $('.lockbottomin');
+
+$biglockin.waypoint(function () {
+  $biglockin.addClass('js-appear-animate');
+}, { offset: '50%'});
+
+$lineupin.waypoint(function () {
+  $lineupin.addClass('js-appear-animate');
+}, { offset: '50%'});
+
+$chartin.waypoint(function () {
+  $chartin.addClass('js-appear-animate');
+}, { offset: '50%'});
+
+$coinin.waypoint(function () {
+  $coinin.addClass('js-appear-animate');
+}, { offset: '50%'});
+
+$bandsin.waypoint(function () {
+  $bandsin.addClass('js-appear-animate');
+}, { offset: '50%'});
+
+$lockbottomin.waypoint(function () {
+  $lockbottomin.addClass('js-appear-animate');
+}, { offset: '50%'});
