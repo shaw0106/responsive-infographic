67dae48a7aa77cb214b6c043fac056716ecdc6f6
Shannon <shaw0106@algonquinlive.com>
2016-04-15 11:24:29 -0400

    bluegroupd gets smaller

--- js/main.js
+++ js/main.js
@@ -7,24 +7,26 @@ var $lockbottomin = $('.lockbottomin');
 
 $biglockin.waypoint(function () {
   $biglockin.addClass('js-appear-animate');
-}, { offset: '50%'});
+}, { offset: '75%'});
 
 $lineupin.waypoint(function () {
   $lineupin.addClass('js-appear-animate');
-}, { offset: '50%'});
+}, { offset: '75%'});
 
 $chartin.waypoint(function () {
   $chartin.addClass('js-appear-animate');
-}, { offset: '50%'});
+}, { offset: '75%'});
 
 $coinin.waypoint(function () {
   $coinin.addClass('js-appear-animate');
-}, { offset: '50%'});
+}, { offset: '75%'});
 
 $bandsin.waypoint(function () {
   $bandsin.addClass('js-appear-animate');
-}, { offset: '50%'});
+}, { offset: '75%'});
 
 $lockbottomin.waypoint(function () {
   $lockbottomin.addClass('js-appear-animate');
-}, { offset: '50%'});
+}, { offset: '75%'});
+
+// expand chart
